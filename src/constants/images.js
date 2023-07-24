abd117188bd39c0db249d6c13e8b7c5d368b649e
tguvenir <tgvnr@protonmail.com>
2023-07-24 20:23:48 +0300

    hero-section & responsive design

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,9 @@
 import Logo from "../assets/blog.svg";
+import Work from "../assets/worktogether.png";
 
 const images = {
   Logo,
+  Work,
 };
 
 export default images;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "../../components/Main";
+import Hero from "./container/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Main>
+        <Hero />
+      </Main>
+    </div>
+  );
+};
+
+export default Home;
